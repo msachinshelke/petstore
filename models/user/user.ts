cced40573d4a0e337cd09e1399a580a89296d513
sachin.shelke@nuance.com <48506478+msachinshelke@users.noreply.github.com>
2024-02-28 15:47:57 +0530

    Intial commit

---
+++ models/user/user.ts
@@ -0,0 +1,10 @@
+export const user = {
+    "id": 0,
+    "username": `test-${Date.now()}`,
+    "firstName": "playwright",
+    "lastName": "testing",
+    "email": "playwright@gustr.com",
+    "password": "playwright@123",
+    "phone": "8080808080",
+    "userStatus": 0
+}
